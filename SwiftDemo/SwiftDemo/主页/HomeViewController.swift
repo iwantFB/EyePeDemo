d4011c10b18bd392d88b6d47c181e318dd3e0368
hufei <hufei@nightstation.cn>
2018-04-04 00:47:43 +0800

    导航选择

--- SwiftDemo/SwiftDemo/主页/HomeViewController.swift
+++ SwiftDemo/SwiftDemo/主页/HomeViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class HomeViewController : UIViewController {
 
+    var naviTopView : HomeNaviCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        p_setupUI()
+    }
+    
+    func p_setupUI() {
         
+        self.view.backgroundColor = UIColor.white
         
-
+        let flowLayout = UICollectionViewFlowLayout.init()
+        flowLayout.itemSize = CGSize.init(width: 60, height: 38)
+        flowLayout.scrollDirection = .horizontal
+        
+        naviTopView = HomeNaviCollectionView.init(frame: CGRect.init(x: 0, y: 0, width: SCREEN_WIDTH, height: 40), collectionViewLayout: flowLayout)
+        naviTopView.showsHorizontalScrollIndicator = false
+        naviTopView.backgroundColor = UIColor.red
+        self.view.addSubview(naviTopView)
+        
     }
     
 }
--- SwiftDemo/SwiftDemo/主页/View/HomeNaviCollectionView.swift
+++ SwiftDemo/SwiftDemo/主页/View/HomeNaviCollectionView.swift
@@ -8,16 +8,67 @@
 
 import UIKit
 
-class HomeNaviCollectionView : UICollectionView {
+class HomeNaciCollectionCell : UICollectionViewCell {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    var titleLb = UILabel.init()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        p_setupUI()
+    }
+    
+    func p_setupUI()  {
+        titleLb.font = UIFont.systemFont(ofSize: 12)
+        titleLb.textAlignment = .center
+        self.contentView.addSubview(titleLb)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        titleLb.frame = self.bounds
+    }
+}
+
+class HomeNaviCollectionView : UICollectionView,UICollectionViewDelegate,UICollectionViewDataSource {
 
     let indexView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: 60, height: 2))
     
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
         super.init(frame: frame, collectionViewLayout: layout)
+        
+        self.register(HomeNaciCollectionCell.self, forCellWithReuseIdentifier: "HomeNaciCollectionCell")
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        indexView.backgroundColor = UIColor.purple
+        self.addSubview(indexView)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeNaciCollectionCell", for: indexPath) as! HomeNaciCollectionCell
+        cell.titleLb.text = "\(indexPath.item)"
+        cell.backgroundColor = UIColor.blue
+        cell.titleLb.textColor = cell.isSelected ? UIColor.red : UIColor.black
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+        let cell = collectionView.cellForItem(at: indexPath)
+        indexView.center = CGPoint.init(x: (cell?.center.x)!, y: 39)
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
 }
